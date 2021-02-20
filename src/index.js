1ff9f8b016d2b953134e0f4b39f3dfba4bd0af52
natashta <natashta@gmail.com>
2021-02-20 20:09:19 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+const teens = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+const tens = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+const hundreds = "";
+const result = "";
+ 
 module.exports = function toReadable (number) {
   
-}
+    let lastNum = number % 10;
+    if (number < 10) {return numbers[number];}
+    else if (lastNum == 0) {return tens[(number / 10) - 1];}
+    else if (number > 10 && number < 20) {return teens[lastNum - 1];}
+
+    }
