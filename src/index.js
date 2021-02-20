c64bba875dcaa3853c4df3cb0705d0c631435a1a
natashta <natashta@gmail.com>
2021-02-20 20:18:23 +0300

    fix: fix solution

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ const result = "";
 module.exports = function toReadable (number) {
   
     let lastNum = number % 10;
+    let secondNum = (number % 100) - (number % 10) ;
     if (number < 10) {return numbers[number];}
     else if (lastNum == 0) {return tens[(number / 10) - 1];}
     else if (number > 10 && number < 20) {return teens[lastNum - 1];}
+    else if (number > 20 && number < 100) {return tens[secondNum - 1] + ' ' + numbers[lustNum];}
 
     }
